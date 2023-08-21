88548747e11aa07de5a6044b39481f9c652ce68b
Syed Ahmed Ali <sahmedali568@gmail.com>
2023-08-20 18:17:44 -0700

    okhy

---
+++ Javascript-assignment/Assignment-3/app.js
@@ -0,0 +1,30 @@
+// Assignment_3
+// chapterNo 14 to 16
+
+// <----Q1---->
+// AnsNo 1:
+// var studentName = [];
+
+// <----Q2---->
+// AnsNo 2:
+// var studentName = [];
+
+// <----Q3---->
+// AnsNo 3:
+// var stringArray = ['ali','aslam','atif'];
+
+// <----Q4---->
+// AnsNo 4:
+// var numberArray = [1,2,3,4,5,6];
+
+// <----Q5---->
+// AnsNo 5:
+// var booleanArray = [true,false,true,false];
+
+// <----Q6---->
+// AnsNo 6:
+// var mixedArray = ['ali',1,true];
+
+// <----Q7---->
+// AnsNo 7:
+
