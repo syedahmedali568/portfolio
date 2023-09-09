d2c651c12e9093c4caa5558a1d9f92c8f54090b6
Syed Ahmed Ali <sahmedali568@gmail.com>
2023-09-10 04:42:46 +0500

    Js-Assign1

---
+++ Javascript-assignment/Assignment-Js-1/app.js
@@ -0,0 +1,247 @@
+// <----Assignment-1---->
+//  <----PDF-1(ALerts)---->#start#
+// Question 1;
+// Write a script to greet your website visitor using JS alert
+// box
+// Answer 1;
+// alert('Welcome to our website');
+
+// Question 2;
+// Write a script to display following message on your web
+// page:
+// Answer 2;
+// alert('Erroe! Please enter a valid password');
+
+// Question 3;
+// Write a script to display following message on your web
+// page: (Hint : Use line break "\n")
+// Answer 3;
+// alert('Welcome to JS Land...\nHappy Coding!');
+
+// Question 4;
+// Write a script to display following messages in sequence:
+// Answer 4;
+// alert('Welcome to JS Land...');
+// alert('Happy Coding!');
+
+// Question 5;
+//Generate the following message through browser’s
+// developer console:
+// Answer 5;
+// console.log("Hello...I can run JS through my web browser's console");
+// #END#
+
+
+// <----PDF-2(VARIABLES FOR STRINGS)---->#start#
+// Question 1;
+// Declare a variable called username.
+// Answer 1;
+// var userName = "Syed Ahmed Ali";
+
+// Question 2;
+// Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// Answer 2;
+// var myName = 'Syed Ahmed Ali';
+
+// Question 3;
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// Answer 3;
+// var message = 'Hello World';
+// alert(message);
+
+// Question 4;
+// Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// Answer 4;
+// var name = 'Syed Ahmed Ali';
+// var age = '19';
+// var Qualification = 'Certified Mobile Application Development'
+// alert(name);
+// alert(age);
+// alert(Qualification);
+
+
+// Question 5;
+// Write a script to display the following alert using one JS
+// variable:
+// Answer 5;
+// alert('PIZZA\nPIZZ\nPIZ\nPI\nP');
+
+// Question 6;
+// Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+// Answer 6;
+// var email = 'example@example.com';
+// alert("My email address is"+' '+email);
+
+
+// Question 7;
+// Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+// Answer 7;
+// var book ='A smarter way to learn JavaScript';
+// alert('I am trying to learn from the Book'+' '+ book);
+
+// Question 8;
+// Write a script to display this in browser through JS
+// Answer 8;
+// document.write('Yah! I can write HTMl content through Javascript');
+
+// Question 9;
+// Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+// Answer 9;
+// alert('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬')
+
+// #END#
+
+// <----PDF-3(VARIABLES FOR NUMBERS)---->#start#
+// Question 1;
+// Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// Answer 1;
+// var age = 'I am 19 years old';
+// alert(age);
+
+// Question 2;
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+// Answer 2;
+// var track = 'You have visited this site 1 time';
+// alert(track);
+// var track = 'You have visited this site 2 times';
+// alert(track);
+// var track = 'You have visited this site 3 times';
+// alert(track);
+// var track = 'You have visited this site 4 times';
+// alert(track);
+// var track = 'You have visited this site 5 times';
+// alert(track);
+// var track = 'You have visited this site 6 times';
+// alert(track);
+// var track = 'You have visited this site 7 times';
+// alert(track);
+// var track = 'You have visited this site 8 times';
+// alert(track);
+// var track = 'You have visited this site 9 times';
+// alert(track);
+// var track = 'You have visited this site 10 times';
+// alert(track);
+// var track = 'You have visited this site 11 times';
+// alert(track);
+// var track = 'You have visited this site 12 times';
+// alert(track);
+// var track = 'You have visited this site 13 times';
+// alert(track);
+// var track = 'You have visited this site 14 times';
+// alert(track);
+// var track = 'Bas krday Chala ja';
+// alert(track);
+
+// Question 3;
+// Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+// Answer 3;
+// var birthYear = "2004";
+// document.write('My birth year is'+' '+birthYear+' '+'<br>Data type of my decleared variable is number');
+
+// Question 4;
+// A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// Answer 4;
+// var VisitorsName = 'Syed Ahmed Ali';
+// var ProductTitle = 'T-Shirt';
+// var Quantity = '5';
+// document.write(VisitorsName +' '+'ordered'+' '+Quantity +' '+ ProductTitle+' '+'j.Clothing store' );
+
+// #END#
+
+// <----PDF-4(VARIABLE NAMES: LEGAL & ILLEGAL)---->#start#
+// Question 1;
+// 1.Declare 3 variables in one statement.
+// Answer 1;
+// var  name = 'Syed Ahmed Ali',age = '19',Qualification = 'Engineer';
+// alert(name);
+// alert(age);
+// alert(Qualification);
+
+// Question 2;
+// 2. Declare 5 legal & 5 illegal variable names.
+// Answer 2:
+// Legal Variable Names:
+// userName
+// _counter
+// $price
+// myVar
+// user123
+
+// Illegal Variable Names:
+// 123user (starts with a number)
+// my-var (contains a hyphen)
+// @username (contains special character other than underscore or dollar sign)
+// for (uses a reserved keyword)
+// let (uses a reserved keyword)
+
+// Question 3;
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________;
+
+// Answer 3:
+// document.write('<h1>Rules for naming JS variables</h1><br>' + 'Variable names can only contain numbers,$ and _ For example $my_1stVariable <br>' + 'Variables must begin with a letter, $ or _.For example $name, _name or name.<br>' + 'Variable names are case sensitive<br>' + 'Variable names should not be JS keywords')
+
+// #END#
+
+// <----PDF-5(MATH EXPRESSIONS)---->#start#
+// Question 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
