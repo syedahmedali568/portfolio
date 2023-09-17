903906e6f6bf07ffacf36eaade60d808c24da553
syedahmedali568 <sahmedali568@gmail.com>
2023-09-17 10:24:50 -0700

    ok

---
+++ Javascript-assignment/Assignment-6/app.js
@@ -0,0 +1,33 @@
+// <---- AssignmentNo-6 ---->
+// <---- CHAPTER FUNCTIONS (35 till 38) ---->
+
+// QuestionNo 01;
+// Write a function that displays current date & time in your
+// browser.
+// AnswerNo 01;
+// function tellTime (){
+//     console.log(new Date())
+// }
+// tellTime();
+
+// QuestionNo 02;
+// Write a function that takes first & last name and then it
+// greets the user using his full name.
+// AnswerNo 02;
+// function greetingUser(firstName,lastName){
+//     console.log(greetingUser)
+// }
+// greetingUser(
+//     prompt("Enter first name:")
+//     prompt("Enter last name:")
+// )
+
+
+
+
+
+
+
+
+
+
